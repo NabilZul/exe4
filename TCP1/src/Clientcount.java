7132e96e3934cf9628109f09718f65212e325ba5
Ahmad Nabil <billonabillo@gmail.com>
2021-04-09 23:22:46 +0800

    Source code exe3

---
+++ TCP1/src/Clientcount.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Clientcount {
+
+	public static void main(String args[]) throws UnknownHostException, IOException
+	{
+		int count;
+		String words;
+		Scanner sc= new Scanner(System.in);
+		Socket s = new Socket("127.0.0.1",1342);
+		Scanner sc1= new Scanner(s.getInputStream());
+		System.out.println("Enter any words: ");
+		words=sc.nextLine();
+		PrintStream p= new PrintStream(s.getOutputStream());
+		p.println(words);
+		
+		count=sc1.nextInt();
+		System.out.println(count);
+	}
+}
